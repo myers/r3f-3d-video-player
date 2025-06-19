8d94740d6cf7060f5ff7e9b549f7540313e92eac
Myers Carpenter <myers@maski.org>
2025-06-19 14:14:18 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import mkcert from "vite-plugin-mkcert"
+
+// https://vitejs.dev/config/
+export default () => {
+  return defineConfig({
+    clearScreen: false,
+    plugins: [react(), mkcert()],
+    server: {
+      https: true,
+    },
+  })
+}
