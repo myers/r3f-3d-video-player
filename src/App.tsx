7c1853efb154bac0ed6fc0893b349ed28bebe58e
Myers Carpenter <myers@maski.org>
2025-06-19 20:38:20 -0400

    import for codesandbox.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber"
+import {
+  PointerEvents,
+  noEvents,
+  createXRStore,
+  XR,
+  XROrigin,
+} from "@react-three/xr"
+import { SplashScreen } from "./SplashScreen"
+import { EquirectPlayer } from "./EquirectPlayer"
 
-function App() {
-  const [count, setCount] = useState(0)
+const store = createXRStore({
+  foveation: 0,
+  layers: true,
+  domOverlay: false,
+  emulate: { inject: true },
+})
 
+export function App() {
+  // This video url is from https://github.com/immersive-web/webxr-samples/blob/main/layers-samples/eqrt-video.html
+  const videoUrl =
+    "https://d25a56pc18k0co.cloudfront.net/sloths_binaural_3840_180_3D-injected.mp4"
+  // const videoUrl = 'MaryOculus.mp4'
+
+  console.log("App")
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SplashScreen store={store}>
+        <h1>VR Video Player</h1>
+        <p>Tested on the Quest Browser. </p>
+        <ul>
+          <li>A button plays and pauses the video</li>
+          <li>
+            Right thumbstick to the right fast forwards the video by 10 seconds
+          </li>
+          <li>
+            Right thumbstick to the left fast forwards the video by 10 seconds
+          </li>
+          <li>B button exits VR</li>
+        </ul>
+      </SplashScreen>
+      <Canvas
+        events={noEvents}
+        style={{ width: "100%", flexGrow: 1 }}
+        camera={{ position: [0, -0.5, 0.5], rotation: [0, 0, 0] }}
+      >
+        <PointerEvents />
+        <XR store={store}>
+          <XROrigin position={[0, -0.5, 0.5]} />
+          <EquirectPlayer videoUrl={videoUrl} videoAngle={180} />
+        </XR>
+      </Canvas>
     </>
   )
 }
-
-export default App
