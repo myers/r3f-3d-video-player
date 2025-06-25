69c9a060ef1f996ce03fe38701e27664f38c405e
Myers Carpenter <myers@maski.org>
2025-06-25 18:29:19 -0400

    sacrifices to appease the storybook gods

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,8 @@ import type { StorybookConfig } from "@storybook/react-vite"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-links",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
   ],
